a1ef67e1903c7a5a16a9c10885f96316dd968a19
lucas-pei <lucaspei39@gmail.com>
2022-04-30 16:20:38 -0400

    created code for 5 learning objectives

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,21 @@
 import processing.core.PApplet;
-
+import processing.core.PImage;  
+/**
+ * Description: A program that demonstrates user input in processing
+ * @author: L. Pei 
+ */
 public class Sketch extends PApplet {
-	
-	
+  //global variables
+  PImage imgKanye;
+  PImage imgGraduation;
+  PImage imgThugger;
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(600, 600);
   }
 
   /** 
@@ -16,21 +23,42 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    // assign images to global image variables
+    imgKanye = loadImage("kanye.png");
+    imgKanye.resize(30, 34);
+
+    imgGraduation = loadImage("graduation.png");
+    imgGraduation.resize(600, 600);
+
+    imgThugger = loadImage("thugger.jpg");
+    imgThugger.resize(600, 600);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    background(0);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    // uses mousePressed variable to change the backgroud to Kanye West's Graduation if the mouse is being pressed
+    if (mousePressed){
+      image(imgGraduation, 0, 0);
+    }
+
+    // uses keyPressed and key variables to output to the user they are pressing a key, and what key it is  
+    if (keyPressed){
+      text("You are currently", 350, 100);
+      text("pressing this key: ", 350, 120);
+      text(key, 445, 120);
+    }
+    
+    // uses mouseX and mouseY variables to draw Kanye West at the position of the mouse
+    fill(255);
+    image(imgKanye, mouseX - 2, mouseY - 5);
+    }
+
   
-  // define other methods down here.
+  public void mouseDragged(){
+    image(imgThugger, 0, 0);
+  }
 }
